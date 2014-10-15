e2312fb8f562e71ae5d65155f1aecd6ea7f4d286
linndroid <linndroid83@gmail.com>
2014-10-15 03:14:36 +0200

    Working filtering and 14 day forecast. Added custom filter. Adding and hiding forecast data to searchResult object on today/14 days toggle.

--- js/Holidayforcast_controllers.js
+++ js/Holidayforcast_controllers.js
@@ -1,26 +1,39 @@
 var weatherApp = angular.module('weatherApp', []);
 
+weatherApp.filter('weatherFilter',function(){
+    return function(input,weathertype) {
+        var returnedInputs = [];
+        console.log("Filtering on " + weathertype);
+        for (var i = 0; i < input.length; i++) {
+            if (input[i].weather!=null && weathertype==input[i].weather[0].main || weathertype==null){
+                returnedInputs.push(input[i]);
+            }
+        }
+        return returnedInputs;
+    };
+});
+
 weatherApp.controller('weatherController', ['$scope', '$http', function ($scope, $http) {
 
-        $scope.apiBaseUrl = "http://api.openweathermap.org/data/2.5/box/city?cluster=yes&bbox=";
+        $scope.apiBBoxBaseUrl = "http://api.openweathermap.org/data/2.5/box/city?cluster=yes&bbox=";
+        $scope.apiForcastBaseUrl ="http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&cnt=14&id=";
         $scope.apiUrl = "";
 
+        <!--Object that stores bounding-box coordinates for locations-->
         $scope.coordinates = {
             'europe':'-24,36,43,72',
             'usa':'-169,12,-52,70'
         }
+        <!--Initialize empty apiCache to store json results from api-->
+        $scope.apiLocationCache = {};
 
-        $scope.apiCache = {};
-        $scope.searchResults = [];
+        <!--This is used to hold the cached results for one search-->
         $scope.cachedResults;
 
-        $scope.apiForcastBaseUrl ="http://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&mode=json"
-        $scope.apiForcastUrl ="";
+        <!--This is iterated in the searchResults table in the view-->
+        $scope.searchResults = [];
 
-        $scope.testCity = "id:1851632";
-        $scope.searchResultForcastJson = '';
-        $scope.forcastSearchResult = [];
-
+        <!--This function iterates the cachedResults and pushes the entries to the searchResults table-->
         $scope.renderView = function(){
             $scope.searchResults = [];
             if ($scope.cachedResults!=null){
@@ -38,18 +51,51 @@ weatherApp.controller('weatherController', ['$scope', '$http', function ($scope,
             }
         }
 
+        $scope.getForecast = function(searchResult, index){
+            console.log("Show weather forecast for " + searchResult.name + " (index="+index+")");
+            $scope.apiUrl = $scope.apiForcastBaseUrl + searchResult.id;
+            console.log("Calling " + $scope.apiUrl);
+
+            responsePromise = $http.get($scope.apiUrl)
+                .success(function(data, status, headers, config) {
+                    $scope.showForecast(data, searchResult, index);
+                })
+                .error(function(data, status, headers, config) {
+                    console.log("Error while getting forecast for " + searchResult.name);
+                });
+        }
+
+        $scope.hideForecast = function(searchResult){
+            searchResult.showForecast = false;
+        }
+
+        $scope.showForecast = function(data,searchResult,index){
+            console.log("Adding forecast for " + searchResult.name + "("+searchResult.id+")");
+            console.log(data);
+            searchResult.forecast=data;
+            searchResult.showForecast=true;
+            /*$scope.searchResults[index].forecast = data;
+            $scope.searchResults[index].showForecast = true;*/
+        }
+
+        <!--Search function the fetches results from api or cache-->
         $scope.search = function(searchQuery) {
 
-            $scope.cachedResults = $scope.apiCache[searchQuery.location];
+            <!--Check if location is in the apiLocationCache-->
+            $scope.cachedResults = $scope.apiLocationCache[searchQuery.location];
 
-            if ($scope.cachedResults==null && $scope.coordinates[searchQuery.location]!=null){
-                $scope.apiUrl = $scope.apiBaseUrl + $scope.coordinates[searchQuery.location];
+            if ($scope.cachedResults == null && $scope.coordinates[searchQuery.location]!=null){
+                $scope.apiUrl = $scope.apiBBoxBaseUrl + $scope.coordinates[searchQuery.location];
                 console.log("Call api " + $scope.apiUrl);
                 responsePromise = $http.get($scope.apiUrl)
                     .success(function(data, status, headers, config) {
-                        console.log('Cache results from api ' + $scope.apiUrl);
+                        console.log('Cacheing results..');
                         $scope.cachedResults = data;
-                        $scope.apiCache[searchQuery.location] = $scope.cachedResults;
+
+                        <!--Put location in the apiLocationCache-->
+                        $scope.apiLocationCache[searchQuery.location] = $scope.cachedResults;
+
+                        <!--Render the view-->
                         $scope.renderView();
                     })
                     .error(function(data, status, headers, config) {
@@ -59,27 +105,9 @@ weatherApp.controller('weatherController', ['$scope', '$http', function ($scope,
 
             if ($scope.cachedResults!=null){
                 console.log("Get searchResults from cache.");
+                <!--Render the view-->
                 $scope.renderView();
             }
+        };
 
-
-                /*
-                 $scope.apiForcastUrl = $scope.apiForcastBaseUrl + $scope.searchCoords;
-                 console.log('Calling remote url ' + $scope.apiForecastUrl + ' to fetch json....')
-                 var responsePromise = $http.get($scope.apiForcastUrl);
-
-                 responsePromise.success(function(data, status, headers, config) {
-                 $scope.searchResultForcastJson = data;
-
-                 var len = $scope.searchResultForcastJson.cnt;
-                 while (len--){
-                 $scope.forcastSearchResult.push($scope.searchResultForcastJson.list[len]);
-                 }
-                 });
-
-                 responsePromise.error(function(data, status, headers, config) {
-                 alert("Calling API failed! " + status);
-                 });*/
-            };
-
-        }]);
+    }]);
