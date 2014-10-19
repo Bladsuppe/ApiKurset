66d22f62fabd9ade44f188128194defc08c31a5b
linn <linndroid83@gmail.com>
2014-10-19 17:12:18 +0200

    Remove unused files. Finish filter on temp_min with html5 range input.

--- js/Holidayforcast_controllers.js
+++ js/Holidayforcast_controllers.js
@@ -3,9 +3,22 @@ var weatherApp = angular.module('weatherApp', []);
 weatherApp.filter('weatherFilter',function(){
     return function(input,weathertype) {
         var returnedInputs = [];
-        console.log("Filtering on " + weathertype);
+        //Filtering on weathertype
         for (var i = 0; i < input.length; i++) {
             if (input[i].weather!=null && weathertype==input[i].weather[0].main || weathertype==null){
+                returnedInputs.push(input[i]);
+            }
+        }
+        return returnedInputs;
+    };
+});
+
+weatherApp.filter('tempFilter',function(){
+    return function(input,tempmin) {
+        var returnedInputs = [];
+        //Filtering on temperature
+        for (var i = 0; i < input.length; i++) {
+            if (input[i].main.temp_min!=null && tempmin<=input[i].main.temp_min || tempmin==null){
                 returnedInputs.push(input[i]);
             }
         }
@@ -16,6 +29,7 @@ weatherApp.filter('weatherFilter',function(){
 weatherApp.controller('weatherController', ['$scope', '$http', function ($scope, $http) {
 
         $scope.loading=false;
+        $scope.tempmin = 20;
 
         $scope.apiBBoxBaseUrl = "http://api.openweathermap.org/data/2.5/box/city?cluster=yes&bbox=";
         $scope.apiForcastBaseUrl ="http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&cnt=14&id=";
@@ -41,23 +55,15 @@ weatherApp.controller('weatherController', ['$scope', '$http', function ($scope,
             if ($scope.cachedResults!=null){
                 var len = $scope.cachedResults.cnt;
                 while (len--){
-                    if (len==1){
-                        console.log("Rendering " + $scope.cachedResults.cnt + " results of format:");
-                        console.log($scope.cachedResults.list[len]);
-                    }
                     $scope.searchResults.push($scope.cachedResults.list[len]);
                 }
-            }
-            else{
-                console.log("No results");
             }
         }
 
         //This function gets the forecast from the api
         $scope.getForecast = function(searchResult, index){
-            console.log("Show weather forecast for " + searchResult.name + " (index="+index+")");
+            //Show weather forecast for " + searchResult.name
             $scope.apiUrl = $scope.apiForcastBaseUrl + searchResult.id;
-            console.log("Calling " + $scope.apiUrl);
             $scope.loading=true;
             responsePromise = $http.get($scope.apiUrl)
                 .success(function(data, status, headers, config) {
@@ -66,7 +72,6 @@ weatherApp.controller('weatherController', ['$scope', '$http', function ($scope,
                 })
                 .error(function(data, status, headers, config) {
                     $scope.loading=false;
-                    console.log("Error while getting forecast for " + searchResult.name);
                 });
         }
 
@@ -75,10 +80,8 @@ weatherApp.controller('weatherController', ['$scope', '$http', function ($scope,
             searchResult.showForecast = false;
         }
 
-        //This function shows the forecast
+        //This function adds the forecast data to the searchResult
         $scope.showForecast = function(data,searchResult,index){
-            console.log("Adding forecast for " + searchResult.name + "("+searchResult.id+")");
-            console.log(data);
             searchResult.forecast=data;
             searchResult.showForecast=true;
         }
@@ -91,12 +94,11 @@ weatherApp.controller('weatherController', ['$scope', '$http', function ($scope,
 
             if ($scope.cachedResults == null && $scope.coordinates[searchQuery.location]!=null){
                 $scope.apiUrl = $scope.apiBBoxBaseUrl + $scope.coordinates[searchQuery.location];
-                console.log("Call api " + $scope.apiUrl);
                 $scope.loading=true;
                 responsePromise = $http.get($scope.apiUrl)
                     .success(function(data, status, headers, config) {
                         $scope.loading=false;
-                        console.log('Cacheing results..');
+                        //Cacheing results
                         $scope.cachedResults = data;
 
                         //Put location in the apiLocationCache
@@ -112,7 +114,6 @@ weatherApp.controller('weatherController', ['$scope', '$http', function ($scope,
             }
 
             if ($scope.cachedResults!=null){
-                console.log("Get searchResults from cache.");
                 //Render the view
                 $scope.renderView();
             }
